7b62c872542d77d54af92e5e67b07f605a8501e3
wcolglazier <139183369+wcolglazier@users.noreply.github.com>
2024-06-19 12:07:49 +0200

    Update new.py

--- new.py
+++ new.py
@@ -35,6 +35,10 @@ def plot_predictions(train_data=X_train,
 weights = np.random.randn(1)
 bias = np.random.randn(1)
 
+# Initial predictions before training
+initial_preds = weights * x_test + bias
+plot_predictions(test_data=x_test, test_labels=y_test, predictions=initial_preds)
+
 # Define learning rate and number of epochs
 learning_rate = 0.0001
 epochs = 80000
